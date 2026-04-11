f39f0568ca87f9adc5e698d028252ff3277a283b
AbhigyanRaj <Abhigyanraj673@gmail.com>
2026-04-11 18:16:16 +0530

    Fix nested git repository in frontend folder

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+
+    <div className="h-screen w-full bg-black flex justify-center items-center">     
+        <h1 className="text-white font-sm">WUP</h1>
+    </div>
+    </>
+  );
+}
